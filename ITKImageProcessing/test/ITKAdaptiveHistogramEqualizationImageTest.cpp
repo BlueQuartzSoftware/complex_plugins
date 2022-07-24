83f5e7bb2111a233229474633945e402b9712582
Michael Jackson <mike.jackson@bluequartz.net>
2022-07-24 10:15:53 -0400

    ITKAdaptiveHistogramEqualizationImage Unit Test Passes.
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- ITKImageProcessing/test/ITKAdaptiveHistogramEqualizationImageTest.cpp
+++ ITKImageProcessing/test/ITKAdaptiveHistogramEqualizationImageTest.cpp
@@ -33,17 +33,66 @@ struct make_shared_enabler : public complex::Application
 {
 };
 
-TEST_CASE("ITKAdaptiveHistogramEqualizationImageFilter(defaults)", "[ITKImageProcessing][ITKAdaptiveHistogramEqualizationImage][defaults]")
+namespace ITKImageProcessingUnitTest
 {
 
-  std::shared_ptr<make_shared_enabler> app = std::make_shared<make_shared_enabler>();
-  app->loadPlugins(unit_test::k_BuildDir.view(), true);
-  auto* filterList = Application::Instance()->getFilterList();
+const std::shared_ptr<make_shared_enabler> app = std::make_shared<make_shared_enabler>();
+bool s_PluginsLoaded = false;
+const FilterList* s_FilterList = nullptr;
 
+void InitApplicationAndPlugins()
+{
+  if(!s_PluginsLoaded)
+  {
+    ITKImageProcessingUnitTest::app->loadPlugins(unit_test::k_BuildDir.view(), true);
+    s_FilterList = Application::Instance()->getFilterList();
+    s_PluginsLoaded = true;
+  }
+}
+
+DataPath ConvertColorToGrayScale(DataStructure& dataStructure, DataPath inputGeometryPath, DataPath inputDataPath)
+{
   const Uuid k_CorePluginId = *Uuid::FromString("65a0a3fc-8c93-5405-8ac6-182e7f313a69");
 
   const Uuid k_ColorToGrayScaleFilterId = *Uuid::FromString("eb5a89c4-4e71-59b1-9719-d10a652d961e");
   const FilterHandle k_ColorToGrayScaleFilterHandle(k_ColorToGrayScaleFilterId, k_CorePluginId);
+
+  // Parameter Keys
+  constexpr StringLiteral k_ConversionAlgorithm_Key = "ConversionAlgorithm";
+  constexpr StringLiteral k_ColorWeights_Key = "ColorWeights";
+  constexpr StringLiteral k_ColorChannel_Key = "ColorChannel";
+  constexpr StringLiteral k_InputDataArrayVector_Key = "InputDataArrayVector";
+  constexpr StringLiteral k_CreateNewAttributeMatrix_Key = "CreateNewAttributeMatrix";
+  constexpr StringLiteral k_OutputAttributeMatrixName_Key = "OutputAttributeMatrixName";
+  constexpr StringLiteral k_OutputArrayPrefix_Key = "OutputArrayPrefix";
+
+  auto filter = s_FilterList->createFilter(k_ColorToGrayScaleFilterHandle);
+  REQUIRE(nullptr != filter);
+
+  Arguments args;
+
+  args.insertOrAssign(k_ConversionAlgorithm_Key, std::make_any<ChoicesParameter::ValueType>(0ULL));
+  args.insertOrAssign(k_ColorWeights_Key, std::make_any<VectorFloat32Parameter::ValueType>({0.2125f, 0.7154f, 0.0721f}));
+  args.insertOrAssign(k_ColorChannel_Key, std::make_any<Int32Parameter::ValueType>(0));
+  args.insertOrAssign(k_InputDataArrayVector_Key, std::make_any<MultiArraySelectionParameter::ValueType>({inputDataPath}));
+  args.insertOrAssign(k_CreateNewAttributeMatrix_Key, std::make_any<BoolParameter::ValueType>(false));
+  args.insertOrAssign(k_OutputAttributeMatrixName_Key, std::make_any<DataGroupCreationParameter::ValueType>(DataPath({"Output Data Path"})));
+  args.insertOrAssign(k_OutputArrayPrefix_Key, std::make_any<StringParameter::ValueType>("GrayScale_"));
+
+  auto preflightResult = filter->preflight(dataStructure, args);
+  COMPLEX_RESULT_REQUIRE_VALID(preflightResult.outputActions);
+
+  auto executeResult = filter->execute(dataStructure, args);
+  COMPLEX_RESULT_REQUIRE_VALID(executeResult.result);
+
+  return inputGeometryPath.createChildPath(fmt::format("GrayScale_{}", ITKTestBase::k_InputDataPath));
+}
+
+} // namespace ITKImageProcessingUnitTest
+
+TEST_CASE("ITKAdaptiveHistogramEqualizationImageFilter(defaults)", "[ITKImageProcessing][ITKAdaptiveHistogramEqualizationImage][defaults]")
+{
+  ITKImageProcessingUnitTest::InitApplicationAndPlugins();
 
   DataStructure dataStructure;
 
@@ -60,37 +109,9 @@ TEST_CASE("ITKAdaptiveHistogramEqualizationImageFilter(defaults)", "[ITKImagePro
   }
 
   // convert color image to grayscale image
-  {
-    // Parameter Keys
-    constexpr StringLiteral k_ConversionAlgorithm_Key = "ConversionAlgorithm";
-    constexpr StringLiteral k_ColorWeights_Key = "ColorWeights";
-    constexpr StringLiteral k_ColorChannel_Key = "ColorChannel";
-    constexpr StringLiteral k_InputDataArrayVector_Key = "InputDataArrayVector";
-    constexpr StringLiteral k_CreateNewAttributeMatrix_Key = "CreateNewAttributeMatrix";
-    constexpr StringLiteral k_OutputAttributeMatrixName_Key = "OutputAttributeMatrixName";
-    constexpr StringLiteral k_OutputArrayPrefix_Key = "OutputArrayPrefix";
+  inputDataPath = ITKImageProcessingUnitTest::ConvertColorToGrayScale(dataStructure, inputGeometryPath, inputDataPath);
 
-    auto filter = filterList->createFilter(k_ColorToGrayScaleFilterHandle);
-    REQUIRE(nullptr != filter);
-
-    Arguments args;
-
-    args.insertOrAssign(k_ConversionAlgorithm_Key, std::make_any<ChoicesParameter::ValueType>(0ULL));
-    args.insertOrAssign(k_ColorWeights_Key, std::make_any<VectorFloat32Parameter::ValueType>({0.2125f, 0.7154f, 0.0721f}));
-    args.insertOrAssign(k_ColorChannel_Key, std::make_any<Int32Parameter::ValueType>(0));
-    args.insertOrAssign(k_InputDataArrayVector_Key, std::make_any<MultiArraySelectionParameter::ValueType>({inputDataPath}));
-    args.insertOrAssign(k_CreateNewAttributeMatrix_Key, std::make_any<BoolParameter::ValueType>(false));
-    args.insertOrAssign(k_OutputAttributeMatrixName_Key, std::make_any<DataGroupCreationParameter::ValueType>(DataPath({"Output Data Path"})));
-    args.insertOrAssign(k_OutputArrayPrefix_Key, std::make_any<StringParameter::ValueType>("GrayScale_"));
-
-    auto preflightResult = filter->preflight(dataStructure, args);
-    COMPLEX_RESULT_REQUIRE_VALID(preflightResult.outputActions);
-
-    auto executeResult = filter->execute(dataStructure, args);
-    COMPLEX_RESULT_REQUIRE_VALID(executeResult.result);
-  }
-
-  inputDataPath = inputGeometryPath.createChildPath(fmt::format("GrayScale_{}", ITKTestBase::k_InputDataPath));
+  // Run the ITK Filter that is being tested.
   {
 
     ITKAdaptiveHistogramEqualizationImage filter;
@@ -129,8 +150,9 @@ TEST_CASE("ITKAdaptiveHistogramEqualizationImageFilter(defaults)", "[ITKImagePro
 
 TEST_CASE("ITKAdaptiveHistogramEqualizationImageFilter(histo)", "[ITKImageProcessing][ITKAdaptiveHistogramEqualizationImage][histo]")
 {
-  DataStructure ds;
-  ITKAdaptiveHistogramEqualizationImage filter;
+  ITKImageProcessingUnitTest::InitApplicationAndPlugins();
+
+  DataStructure dataStructure;
 
   DataPath inputGeometryPath({ITKTestBase::k_ImageGeometryPath});
   DataPath inputDataPath = inputGeometryPath.createChildPath(ITKTestBase::k_InputDataPath);
@@ -138,29 +160,46 @@ TEST_CASE("ITKAdaptiveHistogramEqualizationImageFilter(histo)", "[ITKImageProces
   DataPath outputDataPath = cellDataPath.createChildPath(ITKTestBase::k_OutputDataPath);
 
   { // Start Image Comparison Scope
-    fs::path inputFilePath = fs::path(unit_test::k_SourceDir.view()) / unit_test::k_DataDir.view() / "JSONFilters" / "Input/cthead1.png";
-    Result<> imageReadResult = ITKTestBase::ReadImage(ds, inputFilePath, inputGeometryPath, cellDataPath, inputDataPath);
+    fs::path inputFilePath = fs::path(unit_test::k_SourceDir.view()) / unit_test::k_DataDir.view() / "JSONFilters" / "Input/sf4.png";
+    Result<> imageReadResult = ITKTestBase::ReadImage(dataStructure, inputFilePath, inputGeometryPath, cellDataPath, inputDataPath);
     COMPLEX_RESULT_REQUIRE_VALID(imageReadResult);
   } // End Image Comparison Scope
 
-  Arguments args;
-  args.insertOrAssign(ITKAdaptiveHistogramEqualizationImage::k_SelectedImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
-  args.insertOrAssign(ITKAdaptiveHistogramEqualizationImage::k_SelectedImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
-  args.insertOrAssign(ITKAdaptiveHistogramEqualizationImage::k_OutputImageDataPath_Key, std::make_any<DataPath>(outputDataPath));
-  args.insertOrAssign(ITKAdaptiveHistogramEqualizationImage::k_Alpha_Key, std::make_any<Float32Parameter::ValueType>(0.0));
-  args.insertOrAssign(ITKAdaptiveHistogramEqualizationImage::k_Beta_Key, std::make_any<Float32Parameter::ValueType>(0.0));
+  // convert color image to grayscale image
+  inputDataPath = ITKImageProcessingUnitTest::ConvertColorToGrayScale(dataStructure, inputGeometryPath, inputDataPath);
 
-  auto preflightResult = filter.preflight(ds, args);
-  COMPLEX_RESULT_REQUIRE_VALID(preflightResult.outputActions);
+  // Run the ITK Filter that is being tested.
+  {
+    ITKAdaptiveHistogramEqualizationImage filter;
 
-  auto executeResult = filter.execute(ds, args);
-  COMPLEX_RESULT_REQUIRE_VALID(executeResult.result);
+    Arguments args;
+    args.insertOrAssign(ITKAdaptiveHistogramEqualizationImage::k_SelectedImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
+    args.insertOrAssign(ITKAdaptiveHistogramEqualizationImage::k_SelectedImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
+    args.insertOrAssign(ITKAdaptiveHistogramEqualizationImage::k_OutputImageDataPath_Key, std::make_any<DataPath>(outputDataPath));
+    args.insertOrAssign(ITKAdaptiveHistogramEqualizationImage::k_Radius_Key, std::make_any<VectorFloat32Parameter::ValueType>({10.0F, 10.0F, 10.0F}));
+    args.insertOrAssign(ITKAdaptiveHistogramEqualizationImage::k_Alpha_Key, std::make_any<Float32Parameter::ValueType>(1.0));
+    args.insertOrAssign(ITKAdaptiveHistogramEqualizationImage::k_Beta_Key, std::make_any<Float32Parameter::ValueType>(0.25));
 
-  fs::path baselineFilePath = fs::path(unit_test::k_SourceDir.view()) / complex::unit_test::k_DataDir.view() / "JSONFilters/Baseline/BasicFilters_AdaptiveHistogramEqualizationImageFilter_histo.nrrd";
-  DataPath baselineGeometryPath({ITKTestBase::k_BaselineGeometryPath});
-  DataPath baseLineCellDataPath = baselineGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataPath);
-  DataPath baselineDataPath = baseLineCellDataPath.createChildPath(ITKTestBase::k_BaselineDataPath);
-  Result<> readBaselineResult = ITKTestBase::ReadImage(ds, baselineFilePath, baselineGeometryPath, baseLineCellDataPath, baselineDataPath);
-  Result<> compareResult = ITKTestBase::CompareImages(ds, baselineGeometryPath, baselineDataPath, inputGeometryPath, outputDataPath, 1e-5);
-  COMPLEX_RESULT_REQUIRE_VALID(compareResult);
+    auto preflightResult = filter.preflight(dataStructure, args);
+    COMPLEX_RESULT_REQUIRE_VALID(preflightResult.outputActions);
+
+    auto executeResult = filter.execute(dataStructure, args);
+    COMPLEX_RESULT_REQUIRE_VALID(executeResult.result);
+
+    fs::path baselineFilePath = fs::path(unit_test::k_SourceDir.view()) / complex::unit_test::k_DataDir.view() / "JSONFilters/Baseline/BasicFilters/ITKAdaptiveHistogramEqualizationFilterTest2.png";
+    DataPath baselineGeometryPath({ITKTestBase::k_BaselineGeometryPath});
+    DataPath baseLineCellDataPath = baselineGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataPath);
+    DataPath baselineDataPath = baseLineCellDataPath.createChildPath(ITKTestBase::k_BaselineDataPath);
+    Result<> readBaselineResult = ITKTestBase::ReadImage(dataStructure, baselineFilePath, baselineGeometryPath, baseLineCellDataPath, baselineDataPath);
+    Result<> compareResult = ITKTestBase::CompareImages(dataStructure, baselineGeometryPath, baselineDataPath, inputGeometryPath, outputDataPath, 1e-5);
+    COMPLEX_RESULT_REQUIRE_VALID(compareResult);
+  }
+
+  {
+    Result<H5::FileWriter> result = H5::FileWriter::CreateFile(fmt::format("{}/adaptive_histogram_equalization_histo.dream3d", unit_test::k_BinaryDir));
+    H5::FileWriter fileWriter = std::move(result.value());
+
+    herr_t err = dataStructure.writeHdf5(fileWriter);
+    REQUIRE(err >= 0);
+  }
 }
