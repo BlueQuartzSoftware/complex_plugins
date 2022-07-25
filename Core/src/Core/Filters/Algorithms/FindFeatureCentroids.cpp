a12118e3a448271d88892c49a5ee35e6d5bf55ea
Michael Jackson <mike.jackson@bluequartz.net>
2022-07-25 19:18:19 -0400

    FindFeatureCentroids: Parallel computation for first part of the algorithm
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Core/src/Core/Filters/Algorithms/FindFeatureCentroids.cpp
+++ Core/src/Core/Filters/Algorithms/FindFeatureCentroids.cpp
@@ -3,8 +3,89 @@
 #include "complex/DataStructure/DataArray.hpp"
 #include "complex/DataStructure/DataGroup.hpp"
 #include "complex/DataStructure/Geometry/ImageGeom.hpp"
+#include "complex/Utilities/ParallelDataAlgorithm.hpp"
 
 using namespace complex;
+
+namespace
+{
+
+class FindFeatureCentroidsImpl1
+{
+public:
+  FindFeatureCentroidsImpl1(FindFeatureCentroids* filter, double* sum, double* center, size_t* count, std::array<size_t, 3> dims, const complex::ImageGeom& imageGeom, const Int32Array& featureIds)
+  : m_Filter(filter)
+  , m_Sum(sum)
+  , m_Center(center)
+  , m_Count(count)
+  , m_Dims(dims)
+  , m_ImageGeom(imageGeom)
+  , m_FeatureIds(featureIds)
+  {
+  }
+  ~FindFeatureCentroidsImpl1() = default;
+  void compute(int64_t minFeatureId, int64_t maxFeatureId) const
+  {
+    for(size_t i = 0; i < m_Dims[2]; i++)
+    {
+      size_t zStride = i * m_Dims[0] * m_Dims[1];
+      for(size_t j = 0; j < m_Dims[1]; j++)
+      {
+        size_t yStride = j * m_Dims[0];
+        for(size_t k = 0; k < m_Dims[0]; k++)
+        {
+          int32_t featureId = m_FeatureIds[zStride + yStride + k]; // Get the current FeatureId
+          if(featureId < minFeatureId || featureId >= maxFeatureId)
+          {
+            continue;
+          }
+          complex::Point3Dd voxel_center = m_ImageGeom.getCoords(k, j, i); // Get the voxel center based on XYZ index from Image Geom
+
+          // Kahan Sum for X Coord
+          size_t featureId_idx = featureId * 3ULL;
+          auto componentValue = static_cast<double>(voxel_center[0] - m_Center[featureId_idx]);
+          double temp = m_Sum[featureId_idx] + componentValue;
+          m_Center[featureId_idx] = (temp - m_Sum[featureId_idx]) - componentValue;
+          m_Sum[featureId_idx] = temp;
+          m_Count[featureId_idx]++;
+
+          // Kahan Sum for Y Coord
+          featureId_idx = featureId * 3ULL + 1;
+          componentValue = static_cast<double>(voxel_center[1] - m_Center[featureId_idx]);
+          temp = m_Sum[featureId_idx] + componentValue;
+          m_Center[featureId_idx] = (temp - m_Sum[featureId_idx]) - componentValue;
+          m_Sum[featureId_idx] = temp;
+          m_Count[featureId_idx]++;
+
+          // Kahan Sum for Z Coord
+          featureId_idx = featureId * 3ULL + 2;
+          componentValue = static_cast<double>(voxel_center[2] - m_Center[featureId_idx]);
+          temp = m_Sum[featureId_idx] + componentValue;
+          m_Center[featureId_idx] = (temp - m_Sum[featureId_idx]) - componentValue;
+          m_Sum[featureId_idx] = temp;
+          m_Count[featureId_idx]++;
+        }
+      }
+    }
+  }
+
+  void operator()(const ComplexRange& range) const
+  {
+    compute(range.min(), range.max());
+  }
+
+private:
+  FindFeatureCentroids* m_Filter = nullptr;
+  double* m_Sum = nullptr;
+  double* m_Center = nullptr;
+  size_t* m_Count = nullptr;
+  size_t m_TotalFeatures = 0;
+  std::array<size_t, 3> m_Dims = {0, 0, 0};
+  const complex::ImageGeom& m_ImageGeom;
+  const Int32Array& m_FeatureIds;
+};
+
+} // namespace
 
 // -----------------------------------------------------------------------------
 FindFeatureCentroids::FindFeatureCentroids(DataStructure& dataStructure, const IFilter::MessageHandler& mesgHandler, const std::atomic_bool& shouldCancel, FindFeatureCentroidsInputValues* inputValues)
@@ -27,7 +108,6 @@ const std::atomic_bool& FindFeatureCentroids::getCancel()
 // -----------------------------------------------------------------------------
 Result<> FindFeatureCentroids::operator()()
 {
-
   const auto& featureIds = m_DataStructure.getDataRefAs<Int32Array>(m_InputValues->FeatureIdsArrayPath);
   const auto& imageGeom = m_DataStructure.getDataRefAs<ImageGeom>(m_InputValues->ImageGeometryPath);
   auto& centroids = m_DataStructure.getDataRefAs<Float32Array>(m_InputValues->CentroidsArrayPath);
@@ -38,51 +118,17 @@ Result<> FindFeatureCentroids::operator()()
   size_t yPoints = imageGeom.getNumYPoints();
   size_t zPoints = imageGeom.getNumZPoints();
 
-  // Copy the Coords into an array and then compute the sum
-
   std::vector<double> sum(totalFeatures * 3, 0.0);
   std::vector<double> center(totalFeatures * 3, 0.0);
   std::vector<size_t> count(totalFeatures * 3, 0.0);
 
-  for(size_t i = 0; i < zPoints; i++)
-  {
-    size_t zStride = i * xPoints * yPoints;
-    for(size_t j = 0; j < yPoints; j++)
-    {
-      size_t yStride = j * xPoints;
-      for(size_t k = 0; k < xPoints; k++)
-      {
-        int32_t featureId = featureIds[zStride + yStride + k]; // Get the current FeatureId
+  // The first part can be expensive so parallelize the algorithm
+  ParallelDataAlgorithm dataAlg;
+  dataAlg.setRange(0, totalFeatures);
+  dataAlg.setParallelizationEnabled(true);
+  dataAlg.execute(FindFeatureCentroidsImpl1(this, sum.data(), center.data(), count.data(), {xPoints, yPoints, zPoints}, imageGeom, featureIds));
 
-        complex::Point3Dd voxel_center = imageGeom.getCoords(k, j, i); // Get the voxel center based on XYZ index from Image Geom
-
-        // Kahan Sum for X Coord
-        size_t featureId_idx = featureId * 3ULL;
-        auto componentValue = static_cast<double>(voxel_center[0] - center[featureId_idx]);
-        double temp = sum[featureId_idx] + componentValue;
-        center[featureId_idx] = (temp - sum[featureId_idx]) - componentValue;
-        sum[featureId_idx] = temp;
-        count[featureId_idx]++;
-
-        // Kahan Sum for Y Coord
-        featureId_idx = featureId * 3ULL + 1;
-        componentValue = static_cast<double>(voxel_center[1] - center[featureId_idx]);
-        temp = sum[featureId_idx] + componentValue;
-        center[featureId_idx] = (temp - sum[featureId_idx]) - componentValue;
-        sum[featureId_idx] = temp;
-        count[featureId_idx]++;
-
-        // Kahan Sum for Z Coord
-        featureId_idx = featureId * 3ULL + 2;
-        componentValue = static_cast<double>(voxel_center[2] - center[featureId_idx]);
-        temp = sum[featureId_idx] + componentValue;
-        center[featureId_idx] = (temp - sum[featureId_idx]) - componentValue;
-        sum[featureId_idx] = temp;
-        count[featureId_idx]++;
-      }
-    }
-  }
-
+  // Here we are only looping over the number of features so let this just go in serial mode.
   for(size_t featureId = 0; featureId < totalFeatures; featureId++)
   {
     auto featureId_idx = static_cast<size_t>(featureId * 3);
