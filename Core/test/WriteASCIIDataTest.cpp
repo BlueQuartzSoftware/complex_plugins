0028b0ac2d3ae5ad1598b71f9d55b2a947a0484e
Nathan Young <nathan.young@bluequartz.net>
2022-08-05 16:41:38 -0400

    Almost complete unit test

--- Core/test/WriteASCIIDataTest.cpp
+++ Core/test/WriteASCIIDataTest.cpp
@@ -1,10 +1,11 @@
 #include <catch2/catch.hpp>
 
-#include "Core/Filters/WirteASCIIDataFilter.hpp"
-#include "ComplexCore/Filters/ImportTextFilter.hpp"
+#include "Core/Filters/WriteASCIIDataFilter.hpp"
+#include "complex/Core/Application.hpp"
 
 #include "complex/Common/ScopeGuard.hpp"
 #include "complex/DataStructure/DataArray.hpp"
+#include "complex/DataStructure/IDataArray.hpp"
 #include "complex/DataStructure/DataStore.hpp"
 #include "complex/DataStructure/DataPath.hpp"
 #include "complex/Parameters/ChoicesParameter.hpp"
@@ -16,6 +17,7 @@
 #include "complex/Common/TypesUtility.hpp"
 #include "complex/Common/Types.hpp"
 #include "complex/Filter/Arguments.hpp"
+#include "complex/Filter/FilterHandle.hpp"
 
 #include <filesystem>
 #include <fstream>
@@ -31,16 +33,17 @@ using namespace complex;
 /**
  * @brief The WriteASCIIDataTest class
  */
-namespace Constants
+namespace
 {
-    const fs::path k_TestOutput = fs::path(unit_test::k_BinaryDir.view()) / "WriteASCIIDataTest" / ; // ??? 
+    const fs::path k_TestOutput = fs::path(fmt::format("{}/Testing/Temporary/WriteASCIIDataTest", unit_test::k_BuildDir));
     constexpr int32 k_NumOfDataArrays = 3; // used for generation
+    constexpr int32 k_NumOfTuples = 10; // used for generation
     constexpr int32 k_NumComponents = 16; // used for generation
     constexpr uint64 k_OutputStyleElements = 2; //pull enum # of elements
     constexpr uint64 k_DelimiterElements = 5; //pull enum # of elements
     constexpr uint64 k_MultipleFiles = 0; //enum representation
     constexpr uint64 k_SingleFile = 1; //enum representation
-    constexpr uint64 k_TabDelimiter = 1; //enum representation
+    constexpr uint64 k_TabDelimiter = 4; //enum representation
 
 } // namespace
 
@@ -50,9 +53,9 @@ class RunTest
 {
 public:
     //ctor 
-    RunTest(DataStructure& dataStructure, FilterList* filterList, T fillValue)
+    RunTest(DataStructure& dataStructure, FilterList* filterList)
     : m_DataStructure(dataStructure)
-    , m_FillValue(fillValue)
+    , m_FilterList(filterList)
     {
     }
     //virtual dtor
@@ -61,9 +64,9 @@ public:
     void execute()
     {
         //double for loop for each choice possibility
-        for(uint64 fValue = 0; fValue < Constants::k_OutputStyleElements; fValue++)
+        for(uint64 fValue = 0; fValue < k_OutputStyleElements; fValue++)
         {
-            for (uint64 del = 0; del < Constants::k_DelimiterElements; del++)
+            for (uint64 del = 0; del < k_DelimiterElements; del++)
             {
                 test(fValue, del);
             }
@@ -72,26 +75,23 @@ public:
 
 private:
     DataStructure& m_DataStructure;
-    T m_FillValue;
+    FilterList* m_FilterList;
     const Uuid k_ComplexCorePluginId = *Uuid::FromString("05cc618b-781f-4ac0-b9ac-43f26ce1854f");
     const Uuid k_ImportTextFilterId = *Uuid::FromString("25f7df3e-ca3e-4634-adda-732c0e56efd4");
-    const FilterHandle k_ImportTextFilterHandle(k_ImportTextFilterId, k_ComplexCorePluginId);
+    const FilterHandle k_ImportTextFilterHandle = FilterHandle(k_ImportTextFilterId, k_ComplexCorePluginId);
 
-    std::vector<DataPath> CreateDataArrays() //needs a rework
+    std::vector<DataPath> createDataArrays() //needs a rework
     {
-        std::vector<DataPath> arrayPaths(Constants::k_NumOfDataArrays);
-        for(int32 index = 0; index < Constants::k_NumOfDataArrays; index ++)
+        std::vector<DataPath> arrayPaths(k_NumOfDataArrays);
+        for(int32 index = 0; index < k_NumOfDataArrays; index ++)
         {
-            DataPath array = DataPath({"array_" + index}); //verify proper construction
-            auto& dataArray = m_DataStructure.getDataRefAs<DataArray<T>>(array);
-            auto& dataStore = dataArray.getDataStoreRef();
-            std::fill(dataStore.begin(), dataStore.end(), m_FillValue);
-            arrayPaths.push_back(array);
+            auto* dataArray = UnitTest::CreateTestDataArray(m_DataStructure, "array_" + index, static_cast<DataStore<T>::ShapeType>(std::vector<T>(k_NumOfTuples)), static_cast<DataStore<T>::ShapeType>(std::vector<T>(k_NumComponents)));
+            arrayPaths.push_back(DataPath(dataArray->getName()));
         }
         return arrayPaths;
     }
 
-    void readIn(fs::path filePath, Types::NumericType scalarType, uint64 numTuples, uint64 lineSkip, uint64 delimiter, Datapath path) //review tomorrow
+    void readIn(fs::path filePath, NumericType scalarType, uint64 numTuples, uint64 lineSkip, uint64 delimiter, DataPath path)
     {
         //read in using the Import Text filter
         //swap delimiter index 0 and 2 to match new filter definition
@@ -112,18 +112,18 @@ private:
         static constexpr StringLiteral k_DelimiterChoiceKey = "delimiter_choice";
         static constexpr StringLiteral k_DataArrayKey = "output_data_array";
 
-        auto filter = filterList->createFilter(k_ImportTextFilterHandle);
-        REQUIRE(nullptr != filter);
+        IFilter::UniquePointer filter = m_FilterList->createFilter(k_ImportTextFilterHandle);
+        REQUIRE(filter != nullptr);
 
         Arguments args;
         //args.insertOrAssign(k_InputFileKey, std::make_any<FileSystemPathParameter::ValueType>(fs::path(fmt::format("{}/AlignSectionsFeatureCentroid_1.txt", unit_test::k_BinaryDir))));
         args.insertOrAssign(k_InputFileKey, std::make_any<FileSystemPathParameter::ValueType>(filePath));
-        args.insertOrAssign(k_ScalarTypeKey, std::make_any<NumericTypeParameter::ValueType>(scalarType));
+        args.insertOrAssign(k_ScalarTypeKey, std::make_any<NumericType>(scalarType));
         args.insertOrAssign(k_NTuplesKey, std::make_any<uint64>(numTuples));
         args.insertOrAssign(k_NCompKey, std::make_any<uint64>(k_NumComponents));
         args.insertOrAssign(k_NSkipLinesKey, std::make_any<uint64>(lineSkip));
         args.insertOrAssign(k_DelimiterChoiceKey, std::make_any<ChoicesParameter::ValueType>(delimiter));
-        args.insertOrAssign(k_DataArrayKey, std::make_any<Datapath>(path));
+        args.insertOrAssign(k_DataArrayKey, std::make_any<DataPath>(path));
 
         // Preflight the filter and check result
         auto preflightResult = filter->preflight(m_DataStructure, args);
@@ -134,7 +134,7 @@ private:
         COMPLEX_RESULT_REQUIRE_VALID(executeResult.result)
     }
 
-    void compareArrays(DataArray<T>& originalArray, DataArray<T>& rereadArray)
+    bool compareArrays(IDataArray& originalArray, IDataArray& rereadArray)
     {
         size_t numTuples = originalArray.getNumberOfTuples();
         size_t numTuples2 = rereadArray.getNumberOfTuples();
@@ -150,24 +150,24 @@ private:
                 REQUIRE(originalElement == rereadElement)
             }
         }
+        return true;
     }
 
-    void CompareResults(std::vector<DataPath> daps1, std::vector<DataPath> daps2)
+    void compareResults(std::vector<DataPath> daps1, std::vector<DataPath> daps2)
     {
         for(int32 index = 0; index < static_cast<int32>(daps1.size()); index++)
         {
-            auto& originalArray = m_DataStructure.getDataRefAs<DataArray<T>(daps1[index]);
-            auto& rereadArray = m_DataStructure.getDataRefAs<DataArray<T>(daps2[index]);
+            auto& originalArray = m_DataStructure.getDataRefAs<IDataArray>(daps1[index]);
+            auto& rereadArray = m_DataStructure.getDataRefAs<IDataArray>(daps2[index]);
             REQUIRE(compareArrays(originalArray, rereadArray));
         }
     }
 
     void test(uint64 fileType, uint64 delimiter)
     {
-
         //create data set
         //create DataArrays and store in vector to pass as an args
-        std::vector<DataPath> daps1 = CreateDataArrays();
+        std::vector<DataPath> daps1 = createDataArrays();
 
         // Instantiate the filter and an Arguments Object
         WriteASCIIDataFilter filter;
@@ -175,19 +175,19 @@ private:
 
         // Create default Parameters for the filter.
         args.insertOrAssign(WriteASCIIDataFilter::k_OutputStyle_Key, std::make_any<ChoicesParameter::ValueType>(fileType)); //uint64 0 and 1
-        args.insertOrAssign(WriteASCIIDataFilter::k_OutputPath_Key, std::make_any<FileSystemPathParameter::PathType::OutputDir>(k_TestOutput));
+        args.insertOrAssign(WriteASCIIDataFilter::k_OutputPath_Key, std::make_any<fs::path>(k_TestOutput));
         args.insertOrAssign(WriteASCIIDataFilter::k_FileExtension_Key, std::make_any<StringParameter>(".txt"));
         args.insertOrAssign(WriteASCIIDataFilter::k_MaxValPerLine_Key, std::make_any<Int32Parameter>(1));
         args.insertOrAssign(WriteASCIIDataFilter::k_Delimiter_Key, std::make_any<ChoicesParameter::ValueType>(delimiter)); //uint64 0 - 4 inclusive
         args.insertOrAssign(WriteASCIIDataFilter::k_SelectedDataArrayPaths_Key, std::make_any<MultiArraySelectionParameter::ValueType>(daps1));
 
         // Preflight the filter and check result
-        auto preflightResult = filter.preflight(dataStruct, args);
+        auto preflightResult = filter.preflight(m_DataStructure, args);
         REQUIRE(preflightResult.outputActions.valid());
 
         //print it out to a file
         // Execute the filter and check the result
-        auto executeResult = filter.execute(dataStruct, args);
+        auto executeResult = filter.execute(m_DataStructure, args);
         REQUIRE(executeResult.result.valid());
 
         uint8 type = 0;
@@ -237,39 +237,50 @@ private:
             return; //invalid type
         }
 
-        auto scalarType = static_cast<Types::NumericType>(type);
+        auto scalarType = static_cast<NumericType>(type);
 
         //read the file(s) back in 
         std::vector<DataPath> daps2;
         //calculate numTuples
-        uint64 numTuples = 
+        uint64 numTuples = static_cast<uint64>(k_NumOfTuples);
         //check multi vs single file storage 
-        if(fileType == Constants::k_MultipleFiles) 
+        if(fileType == k_MultipleFiles) 
         {
             for(int32 i = 0; i < daps1.size(); i++)
             {
-                std::string filePath = Constants::k_TestOutput + "array_" + i + ".txt";
-                DataPath array = DataPath({"readArray_" + i}); // review???
-                readIn(filePath, scalarType, numTuples, 0, delimiter, array);
+                auto& selectedArrayPtr = m_DataStructure.getDataRefAs<IDataArray>(daps1[i]);
+                fs::path filePath = fs::path(k_TestOutput.string() + "/" + selectedArrayPtr.getName() + ".txt");
+                DataPath array = DataPath({"readArray_" + i});
+                readIn(filePath, scalarType, numTuples, 0, delimiter, array); // maybe type cast filePath to actual filePathparam?
                 daps2.push_back(array);
             }
         }
-        else if(filetype == Constants::k_SingleFile)
+        else if(fileType == k_SingleFile)
         {
-            std::string filePath = Constants::k_TestOutput + "array_" + i + ".txt";
+            auto& selectedArrayPtr = m_DataStructure.getDataRefAs<IDataArray>(daps1[0]);
+            fs::path filePath = fs::path(k_TestOutput.string() + "/" + selectedArrayPtr.getName() + ".txt");
             uint64 lineSkip = 0;
+            if(delimiter == k_TabDelimiter)
+            {
+                lineSkip = lineSkip + k_NumOfDataArrays + 1; // headers + newline character
+            }
+            else
+            {
+                lineSkip = 2; // header + newline;
+            }
             for(int32 i = 0; i < daps1.size(); i++)
             {
-                DataPath array = DataPath({"readArray_" + i}); // review???
-                readIn(filePath, scalarType, numTuples, , delimiter, array);
+                DataPath array = DataPath({"readArray_" + i});
+                readIn(filePath, scalarType, numTuples, lineSkip, delimiter, array);
                 daps2.push_back(array);
+                lineSkip = lineSkip + numTuples + 1; //add previous lines plus length of previous array and newline character
             }
         }
 
         //compare the read in to the data set
-        CompareResults(daps1, daps2);
+        compareResults(daps1, daps2);
     }
-}
+};
 
 struct make_shared_enabler : public complex::Application
 {
@@ -281,16 +292,17 @@ TEST_CASE("Core::WriteASCIIDataFilter: Auto Generated", "[WriteASCIIDataFilter]"
     app->loadPlugins(unit_test::k_BuildDir.view(), true);
     auto* filterList = Application::Instance()->getFilterList();
 
-    DataStructure& dsRef = DataStructure dataStructure;
+    DataStructure dataStructure;
+    DataStructure& dsRef = dataStructure;
 
-    RunTest<int8>(dsRef, filterList, -2).execute();
-    RunTest<int16>(dsRef, filterList, -29).execute();
-    RunTest<int32>(dsRef, filterList, -2999).execute();
-    RunTest<int64>(dsRef, filterList, -2999999).execute();
-    RunTest<uint8>(dsRef, filterList, 2).execute();
-    RunTest<uint16>(dsRef, filterList, 29).execute();
-    RunTest<uint32>(dsRef, filterList, 2999).execute();
-    RunTest<uint64>(dsRef, filterList, 2999999).execute();
-    RunTest<float32>(dsRef, filterList, 2.9999).execute();
-    RunTest<float64>(dsRef, filterList, 2.999999999).execute(); 
+    RunTest<int8>(dsRef, filterList).execute();
+    RunTest<int16>(dsRef, filterList).execute();
+    RunTest<int32>(dsRef, filterList).execute();
+    RunTest<int64>(dsRef, filterList).execute();
+    RunTest<uint8>(dsRef, filterList).execute();
+    RunTest<uint16>(dsRef, filterList).execute();
+    RunTest<uint32>(dsRef, filterList).execute();
+    RunTest<uint64>(dsRef, filterList).execute();
+    RunTest<float32>(dsRef, filterList).execute();
+    RunTest<float64>(dsRef, filterList).execute(); 
 } // end of test case
