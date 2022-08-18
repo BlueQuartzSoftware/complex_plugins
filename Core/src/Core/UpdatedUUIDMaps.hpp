32e0411e18b4200c7b435e2a2bc1b6473d259f46
Nathan Young <nathan.young@bluequartz.net>
2022-08-18 12:13:39 -0400

    Added the autogenerated map files after a touch-up to make functional maps

---
+++ Core/src/Core/UpdatedUUIDMaps.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <map>
+#include<iterator>
+#include <string>
+
+namespace complex
+{
+
+class UpdatedUUIDMaps
+{
+public:
+  UpdatedUUIDMaps()
+  {
+  }
+  ~UpdatedUUIDMaps() = default;
+  UpdatedUUIDMaps(const UpdatedUUIDMaps&) = default;
+  UpdatedUUIDMaps(UpdatedUUIDMaps&&) noexcept = default;
+  UpdatedUUIDMaps& operator=(const UpdatedUUIDMaps&) = delete;
+  UpdatedUUIDMaps& operator=(UpdatedUUIDMaps&&) noexcept = delete;
+  
+private:
+  static const std::map<std::string, std::string> k_SIMPL_to_Complex;
+  static const std::map<std::string, std::string> k_Complex_to_SIMPL;
+};
+
+} // namespace complex
