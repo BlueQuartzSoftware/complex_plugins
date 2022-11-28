8cebd8d151f715fec5eff7dfa4069ab107d9358c
Jessica Marquis <jessica.marquis@bluequartz.net>
2022-11-28 16:30:00 -0500

    Add CreateEnsembleInfoFilter

---
+++ OrientationAnalysis/src/OrientationAnalysis/Filters/CreateEnsembleInfoFilter.cpp
@@ -0,0 +1,121 @@
+#include "CreateEnsembleInfoFilter.hpp"
+
+#include "complex/Common/Types.hpp"
+#include "complex/DataStructure/DataPath.hpp"
+#include "complex/Filter/Actions/CreateArrayAction.hpp"
+#include "complex/Filter/Actions/CreateAttributeMatrixAction.hpp"
+#include "complex/Filter/Actions/CreateStringArrayAction.hpp"
+#include "complex/Parameters/DataGroupCreationParameter.hpp"
+#include "complex/Parameters/DataObjectNameParameter.hpp"
+#include "complex/Parameters/EnsembleInfoParameter.hpp"
+#include "complex/Parameters/GeometrySelectionParameter.hpp"
+
+#include "OrientationAnalysis/Filters/Algorithms/CreateEnsembleInfo.hpp"
+
+using namespace complex;
+
+namespace complex
+{
+//------------------------------------------------------------------------------
+std::string CreateEnsembleInfoFilter::name() const
+{
+  return FilterTraits<CreateEnsembleInfoFilter>::name.str();
+}
+
+//------------------------------------------------------------------------------
+std::string CreateEnsembleInfoFilter::className() const
+{
+  return FilterTraits<CreateEnsembleInfoFilter>::className;
+}
+
+//------------------------------------------------------------------------------
+Uuid CreateEnsembleInfoFilter::uuid() const
+{
+  return FilterTraits<CreateEnsembleInfoFilter>::uuid;
+}
+
+//------------------------------------------------------------------------------
+std::string CreateEnsembleInfoFilter::humanName() const
+{
+  return "Create Ensemble Info";
+}
+
+//------------------------------------------------------------------------------
+std::vector<std::string> CreateEnsembleInfoFilter::defaultTags() const
+{
+  return {"#Processing", "#Generation"};
+}
+
+//------------------------------------------------------------------------------
+Parameters CreateEnsembleInfoFilter::parameters() const
+{
+  Parameters params;
+  // Create the parameter descriptors that are needed for this filter
+  params.insert(std::make_unique<GeometrySelectionParameter>(k_GeometryName_Key, "Geometry", "The complete path to the input geometry", DataPath{}, IGeometry::GetAllGeomTypes()));
+  params.insert(std::make_unique<EnsembleInfoParameter>(
+      k_Ensemble_Key, "Created Ensemble Info", "The values with which to populate the crystal structures, phase types, and phase names data arrays. Each row corresponds to an ensemble phase.",
+      EnsembleInfoParameter::ValueType{}));
+  params.insert(std::make_unique<DataGroupCreationParameter>(k_CellEnsembleAttributeMatrixName_Key, "Ensemble Attribute Matrix",
+                                                             "The complete path to the attribute matrix in which to store the ensemble phase data arrays", DataPath{}));
+  params.insert(std::make_unique<DataObjectNameParameter>(k_CrystalStructuresArrayName_Key, "Crystal Structures", "The name of the data array representing the crystal structure for each Ensemble",
+                                                          DataObjectNameParameter::ValueType{}));
+  params.insert(std::make_unique<DataObjectNameParameter>(k_PhaseTypesArrayName_Key, "Phase Types", "The name of the data array representing the phase types for each Ensemble",
+                                                          DataObjectNameParameter::ValueType{}));
+  params.insert(std::make_unique<DataObjectNameParameter>(k_PhaseNamesArrayName_Key, "Phase Names", "The name of the string array representing the phase names for each Ensemble",
+                                                          DataObjectNameParameter::ValueType{}));
+
+  return params;
+}
+
+//------------------------------------------------------------------------------
+IFilter::UniquePointer CreateEnsembleInfoFilter::clone() const
+{
+  return std::make_unique<CreateEnsembleInfoFilter>();
+}
+
+//------------------------------------------------------------------------------
+IFilter::PreflightResult CreateEnsembleInfoFilter::preflightImpl(const DataStructure& dataStructure, const Arguments& filterArgs, const MessageHandler& messageHandler,
+                                                                 const std::atomic_bool& shouldCancel) const
+{
+  auto pDataContainerNameValue = filterArgs.value<DataPath>(k_GeometryName_Key);
+  auto pEnsembleValue = filterArgs.value<EnsembleInfoParameter::ValueType>(k_Ensemble_Key);
+  auto pCellEnsembleAttributeMatrixNameValue = filterArgs.value<DataPath>(k_CellEnsembleAttributeMatrixName_Key);
+  auto pCrystalStructuresArrayNameValue = filterArgs.value<DataObjectNameParameter::ValueType>(k_CrystalStructuresArrayName_Key);
+  auto pPhaseTypesArrayNameValue = filterArgs.value<DataObjectNameParameter::ValueType>(k_PhaseTypesArrayName_Key);
+  auto pPhaseNamesArrayNameValue = filterArgs.value<DataObjectNameParameter::ValueType>(k_PhaseNamesArrayName_Key);
+
+  PreflightResult preflightResult;
+  OutputActions outputActions;
+  std::vector<PreflightValue> preflightUpdatedValues;
+
+  int numPhases = pEnsembleValue.size();
+  std::vector<usize> tDims(1, numPhases + 1);
+  auto createAttributeMatrixAction = std::make_unique<CreateAttributeMatrixAction>(pCellEnsembleAttributeMatrixNameValue, tDims);
+  outputActions.actions.push_back(std::move(createAttributeMatrixAction));
+
+  std::vector<size_t> cDims(1, 1);
+  auto createCrystalStructuresAction = std::make_unique<CreateArrayAction>(DataType::uint32, tDims, cDims, pCellEnsembleAttributeMatrixNameValue.createChildPath(pCrystalStructuresArrayNameValue));
+  auto createPhaseTypesAction = std::make_unique<CreateArrayAction>(DataType::uint32, tDims, cDims, pCellEnsembleAttributeMatrixNameValue.createChildPath(pPhaseTypesArrayNameValue));
+  auto createPhaseNamesAction = std::make_unique<CreateStringArrayAction>(tDims, pCellEnsembleAttributeMatrixNameValue.createChildPath(pPhaseNamesArrayNameValue));
+  outputActions.actions.push_back(std::move(createCrystalStructuresAction));
+  outputActions.actions.push_back(std::move(createPhaseTypesAction));
+  outputActions.actions.push_back(std::move(createPhaseNamesAction));
+
+  return {std::move(outputActions)};
+}
+
+//------------------------------------------------------------------------------
+Result<> CreateEnsembleInfoFilter::executeImpl(DataStructure& dataStructure, const Arguments& filterArgs, const PipelineFilter* pipelineNode, const MessageHandler& messageHandler,
+                                               const std::atomic_bool& shouldCancel) const
+{
+  CreateEnsembleInfoInputValues inputValues;
+  inputValues.DataContainerName = filterArgs.value<DataPath>(k_GeometryName_Key);
+  inputValues.Ensemble = filterArgs.value<EnsembleInfoParameter::ValueType>(k_Ensemble_Key);
+  inputValues.CellEnsembleAttributeMatrixName = filterArgs.value<DataPath>(k_CellEnsembleAttributeMatrixName_Key);
+  inputValues.CrystalStructuresArrayName = filterArgs.value<DataObjectNameParameter::ValueType>(k_CrystalStructuresArrayName_Key);
+  inputValues.PhaseTypesArrayName = filterArgs.value<DataObjectNameParameter::ValueType>(k_PhaseTypesArrayName_Key);
+  inputValues.PhaseNamesArrayName = filterArgs.value<DataObjectNameParameter::ValueType>(k_PhaseNamesArrayName_Key);
+
+  return CreateEnsembleInfo(dataStructure, messageHandler, shouldCancel, &inputValues)();
+}
+} // namespace complex
