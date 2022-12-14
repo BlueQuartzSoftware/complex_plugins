55f7c292f28846806e56fb9f34c3aed3822fa241
Nathan Young <nathan.young@bluequartz.net>
2022-12-14 15:45:41 -0500

    alter the AlignSectionsFeatureCentroidTest

--- Core/test/AlignSectionsFeatureCentroidTest.cpp
+++ Core/test/AlignSectionsFeatureCentroidTest.cpp
@@ -16,6 +16,7 @@
 #include <filesystem>
 namespace fs = std::filesystem;
 
+#include "complex/UnitTest/UnitTestCommon.hpp"
 #include "complex_plugins/Utilities/SmallIN100Utilities.hpp"
 #include "complex_plugins/Utilities/TestUtilities.hpp"
 
